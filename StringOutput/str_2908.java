8b470cdcdc6cfc68bec0ba664a2cc0eb4b0dc7fa
pokavv <okaps@hanmail.net>
2023-11-16 00:31:04 +0900

    231116 baekjoon

---
+++ StringOutput/str_2908.java
@@ -0,0 +1,22 @@
+package StringOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class str_2908 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.println(max(a, b));
+    }
+}
