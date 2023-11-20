91bf6f385a2c5f8d49be7a703211b84d122effc1
pokavv <okaps@hanmail.net>
2023-11-21 00:14:44 +0900

    231120 baekjoon

--- Advanced1/adv_25206.java
+++ Advanced1/adv_25206.java
@@ -8,60 +8,34 @@ import java.util.StringTokenizer;
 public class adv_25206 {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        double sumNumScore = 0;
-        double sumNum = 0;
+
+        String str[] = new String[20];
+        double totalSum = 0;
+        double scoreSum = 0;
+        String gradeList[] = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F", "P"};
+        double gradeScore[] = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0, 0.0};
 
         for (int i = 0; i < 20; i++) {
-            st = new StringTokenizer(br.readLine());
-            String obj = st.nextToken();
-            double num = Double.parseDouble(st.nextToken());
-            String alp = st.nextToken();
-            double score = 0;
+            str[i] = br.readLine();
+            StringTokenizer st = new StringTokenizer(str[i], " ");
+            String subject = st.nextToken();
+            double score = Double.parseDouble(st.nextToken());
+            String grade = st.nextToken();
 
-            // P가 아닐 때만 체크
-            if (!(alp.equals("P"))) {
-                switch (alp) {
-                    case "A+":
-                        score = 4.5;
-                        break;
-                    case "A0":
-                        score = 4.0;
-                        break;
-                    case "B+":
-                        score = 3.5;
-                        break;
-                    case "B0":
-                        score = 3.0;
-                        break;
-                    case "C+":
-                        score = 2.5;
-                        break;
-                    case "C0":
-                        score = 2.0;
-                        break;
-                    case "D+":
-                        score = 1.5;
-                        break;
-                    case "D0":
-                        score = 1.0;
-                        break;
-                    case "F":
-                        score = 0.0;
-                        break;
-                    default:
-                        break;
+            for (int j = 0; j < 10; j++) {
+                if (grade.equals(gradeList[j])) {
+                    totalSum += score * gradeScore[j];
+                    if (j != 9) {
+                        scoreSum += score;
+                    }
                 }
-
-                sumNumScore += (num + score);
-                sumNum += num;
             }
         }
 
-        // 소수점 아래 6자리까지 출력
-        System.out.printf("%.6f", sumNumScore/sumNum);
+        double average = totalSum / scoreSum;
+        System.out.printf("%.6f\n", average);
+
         br.close();
     }
 }
