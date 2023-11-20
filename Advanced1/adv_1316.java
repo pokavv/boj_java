39e1a32174714e44e0ddf0bbf683a4704699a4d9
pokavv <okaps@hanmail.net>
2023-11-20 23:59:19 +0900

    231120 baekjoon

--- Advanced1/adv_1316.java
+++ Advanced1/adv_1316.java
@@ -6,9 +6,10 @@ import java.io.InputStreamReader;
 
 public class adv_1316 {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int cnt = 0;
 
@@ -23,28 +24,25 @@ public class adv_1316 {
 
     public static boolean check() throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         boolean[] check = new boolean[26];
         int prev = 0;
 
         for (int i = 0; i < str.length(); i++) {
-            char now = str.charAt(i); // i 번째 문자 저장 (현재 문자)
+            int now = str.charAt(i);
 
-            // 이전 문자와 i 번째 문자가 같지 않다면?
+            // 앞선 문자와 i 번째 문자가 같지 않다면?
             if (prev != now) {
-                // 해당 문자가 처음 나오는 경우 (false)
+                // 해당 문자가 처음 나오는 경우 (false 인 경우)
                 if (check[now - 'a'] == false) {
-                    check[now - 'a'] = true; // true 로 바꾼다
-                    prev = now; // 다음 턴을 위해 prev 도 true 로 바꾼다
+                    check[now - 'a'] = true;
+                    prev = now;
                 }
-
-                // 해당 문자가 이미 나온 적이 있는 경우 (그룹 단어가 아니게 됨)
+                // 해당 문자가 이미 나온 적이 있는 경우 (그룹단어가 아니게 됨)
                 else {
                     return false; // 함수 종료
                 }
             }
-
             // 앞선 문자와 i 번째 문자가 같다면? (연속된 문자)
             continue;
         }
--- Advanced1/adv_25206.java
+++ Advanced1/adv_25206.java
@@ -1,4 +1,67 @@
 package Advanced1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class adv_25206 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        double sumNumScore = 0;
+        double sumNum = 0;
+
+        for (int i = 0; i < 20; i++) {
+            st = new StringTokenizer(br.readLine());
+            String obj = st.nextToken();
+            double num = Double.parseDouble(st.nextToken());
+            String alp = st.nextToken();
+            double score = 0;
+
+            // P가 아닐 때만 체크
+            if (!(alp.equals("P"))) {
+                switch (alp) {
+                    case "A+":
+                        score = 4.5;
+                        break;
+                    case "A0":
+                        score = 4.0;
+                        break;
+                    case "B+":
+                        score = 3.5;
+                        break;
+                    case "B0":
+                        score = 3.0;
+                        break;
+                    case "C+":
+                        score = 2.5;
+                        break;
+                    case "C0":
+                        score = 2.0;
+                        break;
+                    case "D+":
+                        score = 1.5;
+                        break;
+                    case "D0":
+                        score = 1.0;
+                        break;
+                    case "F":
+                        score = 0.0;
+                        break;
+                    default:
+                        break;
+                }
+
+                sumNumScore += (num + score);
+                sumNum += num;
+            }
+        }
+
+        // 소수점 아래 6자리까지 출력
+        System.out.printf("%.6f", sumNumScore/sumNum);
+        br.close();
+    }
 }
