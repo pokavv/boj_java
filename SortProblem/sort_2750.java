59d2ffa897cef4ee7330978f150ef8e760274628
pokavv <okaps@hanmail.net>
2024-01-30 23:57:48 +0900

    240130 star

---
+++ SortProblem/sort_2750.java
@@ -0,0 +1,19 @@
+package SortProblem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sort_2750 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int row = Integer.parseInt(br.readLine());
+        for (int i = 0; i < row; i++) {
+
+        }
+    }
+}
